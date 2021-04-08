5761b54a91bd4cbcfff6e2f60aa762de7922fe35
abu-al3ees <malkhazali83@gmail.com>
2021-04-08 23:03:01 +0300

    add 5 herbal

---
+++ herbal.sql
@@ -0,0 +1,15 @@
+(name, img_url, case_using, Preparation, description)
+('Lime flower', 'https://p0.pikist.com/photos/857/117/lime-flower-white-flower-lime-white-flower-green-nature-leaf-yellow.jpg','Cough and cold, Mental stress and mood disorders', 'By soaking the lemon blossom', 'Lemon blossom is part of the lemon tree, which is one of the most popular citrus trees grown in home gardens and orchards'),
+
+
+('Guarana seed', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS06CTqkOsQCczdz0K-YmFdNt48TTEkehgPlA&usqp=CAU','Fatigue and weakness and  used for weight loss','Oven cook','A plant originating from the Brazilian Amazon, its seeds are characterized by a high concentration of caffeine'),
+
+('Yarrow','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAa9P5XHi6HHl2eOUWQKXOjO_UfinlEokdPw&usqp=CAU','Loss of appetite, Skin disorders and minor wounds, Urinary tract and genital disorders, Gastrointestinal disorders','Put two tablespoons of the yarrow herb, and pour it into the bowl with a bottle of hot boiled water, and take 4 cups half an hour before starting each meal','Yarrow is an erect, herbaceous, perennial plant that produces one to several stems'),
+
+
+
+('Peppermint oil','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScCkUnXSrA5aCEdGA_3FSc9-BR1gQFI06z6g&usqp=CAU', 'Pain and inflammation, Skin disorders and minor wounds, Cough and cold, Gastrointestinal disorders','Wash it, clean it and add vegetable oil to it','It is an essential oil that is extracted from the leaves of the mint plant for a variety of different purposes'),
+
+
+
+('peppermint leaf', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvyNfFZdtH6UCqDyIKVtgsXNFhX-TqWC0wdA&usqp=CAU', 'Gastrointestinal disorders','Boil it and drink it','It is a genus of plants that follows the phyla of the order Labiala'),
